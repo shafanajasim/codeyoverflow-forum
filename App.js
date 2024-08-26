9c2347d0ba99b78ee0889761214040c3de21e382
shafanajasim <145493633+shafanajasim@users.noreply.github.com>
2024-08-26 19:01:36 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {comments} from './commentData'
+import Card from './Card'
+
+function App() {
+  return (
+    <div>
+      {comments.map(comment => (
+        <Card key={comment.username} commentObject={comment} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
